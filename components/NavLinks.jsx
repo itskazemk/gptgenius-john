671f04b6783f39677952e3220af7862530d60ca9
Kazem Kamali <itskazemk@gmail.com>
2024-07-21 14:52:58 +0330

    012 Nav Links

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
+const links = [
+  { label: "chat", href: "/chat" },
+  { label: "tours", href: "/tours" },
+  { label: "new tour", href: "/tours/new-tour" },
+  { label: "profile", href: "/profile" },
+];
+
 const NavLinks = () => {
-	return <div>NavLinks</div>;
+  return (
+    <ul className="menu text-base-content">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link href={link.href} className="capitalize">
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default NavLinks;
